49f5b3047f4bcd3a5d3dced920f029c45123d055
Samrat Kumar Das <90477654+colddsam@users.noreply.github.com>
2023-05-30 16:56:06 +0530

    Create 705. Design HashSet.cpp

---
+++ 705. Design HashSet.cpp
@@ -0,0 +1,17 @@
+int arr[10000001];
+    
+    MyHashSet() {
+        memset(arr,-1,sizeof(arr));
+    }
+    
+    void add(int key) {
+        arr[key]=1;
+    }
+    
+    void remove(int key) {
+        arr[key]=-1;
+    }
+    
+    bool contains(int key) {
+        return arr[key]==1;
+    }
