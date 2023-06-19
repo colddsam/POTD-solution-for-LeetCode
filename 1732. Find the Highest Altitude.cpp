c50929022aa565e2fabea69b201d352cb31238c2
Samrat Kumar Das <90477654+colddsam@users.noreply.github.com>
2023-06-19 23:45:03 +0530

    Create 1732. Find the Highest Altitude.cpp

---
+++ 1732. Find the Highest Altitude.cpp
@@ -0,0 +1,12 @@
+int largestAltitude(vector<int>& gain) {
+        int cur = 0;
+        int highest = cur;
+
+        for (int i = 0; i < gain.size(); i++) {
+            int a = gain[i];
+            cur += a;
+            highest = max(highest, cur);
+        }
+
+        return highest;        
+    }
